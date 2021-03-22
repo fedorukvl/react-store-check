5e2041028dc795e6d6b1dc7036a81ac0e5b1b190
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-22 11:25:29 +0300

    Добавлен header приложения

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <header>
+        <h1 className="app__title">Whats in store</h1>
+      </header>
     </div>
   );
 }
