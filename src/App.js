5c8d61fc611073477561227086275acf67932e2c
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-25 18:49:37 +0300

    Сверстан компонент Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AuthForm from './Components/Auth/Auth-form';
+import Navigation from './Components/Navigation/Navigation';
 import ResultWindow from './Components/ResultWindow/Result-window';
 
 function App() {
   return (
     <div className="app">
+      <nav><Navigation/></nav>
       <header>
         <h1 className="app__title">Whats in store</h1>
       </header>
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <ul className="navigation__links">
+        <li className="navigation__link">What's in store</li>
+        <li className="navigation__link">About</li>
+        <li className="navigation__link">What's in route</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
