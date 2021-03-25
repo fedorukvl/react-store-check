d5d651fcedbda3cd99e913617626e7c3406d67cb
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-25 14:55:23 +0300

    Добавлены стили компоненту AuthForm

--- src/Components/Auth/Auth-form.jsx
+++ src/Components/Auth/Auth-form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../media/css/Auth-form.css";
 
 function AuthForm() {
   return (
@@ -7,13 +8,18 @@ function AuthForm() {
         <label htmlFor="login" className="auth__login-label">
           Логин
         </label>
-        <input type="text" id="login" name="login" className="auth__logi" />
+        <input type="text" id="login" name="login" className="auth__login" />
       </div>
       <div className="auth__password-info">
         <label htmlFor="password" className="auth__password-label">
           Пароль
         </label>
-        <input type="text" id="password" name="password" />
+        <input
+          type="text"
+          id="password"
+          name="password"
+          className="auth__password"
+        />
       </div>
     </form>
   );
