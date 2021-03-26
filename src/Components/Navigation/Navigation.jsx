c9f119672ef3ee44fbcfde3232afd10dae8a296d
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-26 16:55:24 +0300

    Добавлены ссылки для маршрутизации в компонент Navigation

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../media/css/Navigation.css";
 
 function Navigation() {
   return (
     <div className="navigation">
       <ul className="navigation__links">
-        <li className="navigation__link">What's in store</li>
-        <li className="navigation__link">About</li>
-        <li className="navigation__link">What's in route</li>
+        <Link to="/store" className="navigation__link">
+          What's in store
+        </Link>
+        <Link to="/about" className="navigation__link">
+          About
+        </Link>
+        <Link to="/route" className="navigation__link">
+          What's in route
+        </Link>
       </ul>
     </div>
   );
